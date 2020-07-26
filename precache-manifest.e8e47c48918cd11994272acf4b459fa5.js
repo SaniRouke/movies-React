8cfbb566fa5b9516d1d1ecf199e2762646427a6d
Rustam <Xpyctam4@yandex.ru>
2020-07-26 19:43:10 +0400

    Updates

---
+++ precache-manifest.e8e47c48918cd11994272acf4b459fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8917ab97753120c519ea7c38ab5f42d",
+    "url": "/movies-React/index.html"
+  },
+  {
+    "revision": "3d672528b8cb24397c7d",
+    "url": "/movies-React/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "82f71e8ebcde3dcc2180",
+    "url": "/movies-React/static/js/2.be61bf59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movies-React/static/js/2.be61bf59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d672528b8cb24397c7d",
+    "url": "/movies-React/static/js/main.9c3da032.chunk.js"
+  },
+  {
+    "revision": "eeeffe9ff1bbb09e1ee9",
+    "url": "/movies-React/static/js/runtime-main.6d4d22c8.js"
+  }
+]);
