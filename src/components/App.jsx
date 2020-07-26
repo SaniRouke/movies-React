b7bd65d9021f85e0ea6f9c7fd0cfe8dfe0938536
Rustam <Xpyctam4@yandex.ru>
2020-07-26 19:52:52 +0400

    test commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Movies</div>;
+  return <div className="App">Movies updated</div>;
 }
 
 export default App;
