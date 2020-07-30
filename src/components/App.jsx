371b012a0931b3b178fb944cdb1ad6af4040e96b
Rustam <Xpyctam4@yandex.ru>
2020-07-30 22:58:58 +0400

    + spinner, fix css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,34 +2,47 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import "antd/dist/antd.css";
 import moviesApi from "../services/moviesApi";
-import { Layout, Tabs } from "antd";
+import { Layout, Tabs, Spin } from "antd";
 import Search from "./Search";
 import MoviesList from "./MoviesList";
 import MovieItem from "./MovieItem";
 
 function App() {
-  const [sessionID, setSessionID] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [sessionID, setSessionID] = useState(null);
   const [sort, setSort] = useState("search");
   const [data, setData] = useState([]);
   const [currentPage, setPage] = useState(1);
 
   useEffect(() => {
     console.log("useEffect init");
-    async function asyncFunc() {
-      const session = await moviesApi.createGuestSession();
-      await setSessionID(session.guest_session_id);
-    }
-    asyncFunc();
+    Promise.resolve()
+      .then(() => {
+        return moviesApi.createGuestSession();
+      })
+      .then((session) => {
+        setSessionID(session.guest_session_id);
+        return;
+      });
   }, []);
 
   useEffect(() => {
     console.log("useEffect Page");
     loadData();
+    window.scrollTo(0, 0);
   }, [currentPage, sort]);
 
-  async function loadData(query) {
-    const list = await moviesApi.getSearchList(sessionID, query, currentPage);
-    setData(list.results);
+  function loadData(query) {
+    setLoading(true);
+    Promise.resolve()
+      .then(() => {
+        return moviesApi.getSearchList(sessionID, query, currentPage);
+      })
+      .then(({ results }) => {
+        setLoading(false);
+        setData(results);
+        return results;
+      });
   }
 
   const { Content } = Layout;
@@ -49,17 +62,19 @@ function App() {
             <TabPane tab="Rated" key="rated" />
           </Tabs>
           <Search loadData={loadData} disabled={sort === "rated"} />
-          <MoviesList
-            className="MoviesList"
-            currentPage={currentPage}
-            setPage={(page) => setPage(page)}
-          >
-            {data.map((movie) => (
-              <li className="li-MovieItem" key={movie.id}>
-                <MovieItem className="MovieItem" data={movie} />
-              </li>
-            ))}
-          </MoviesList>
+          <Spin spinning={loading} size="large">
+            <MoviesList
+              className="MoviesList"
+              currentPage={currentPage}
+              setPage={(page) => setPage(page)}
+            >
+              {data.map((movie) => (
+                <li className="li-MovieItem" key={movie.id}>
+                  <MovieItem className="MovieItem" data={movie} />
+                </li>
+              ))}
+            </MoviesList>
+          </Spin>
         </Content>
       </Layout>
     </div>
